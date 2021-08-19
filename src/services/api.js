b9fdcfa6c3305e497fa234c0e3158301921c2a25
unknown <999barbell@gmail.com>
2021-08-19 20:15:04 +0300

    hw4-images-hooks

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+
+axios.defaults.baseURL = "https://pixabay.com/api/";
+const KEY = "21824668-10aeb8c8af54ec25684dd6884";
+
+const fetchHits = ({ searchQuery = "", currentPage = 1, pageSize = 12 }) => {
+  return axios
+    .get(
+      `/?key=${KEY}&q=${searchQuery}&page=${currentPage}&image_type=photo&orientation=horizontal&per_page=${pageSize}`
+    )
+    .then((response) => response.data.hits);
+};
+
+
+export { fetchHits };
